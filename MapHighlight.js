d8b050c912f761634c07c60639eab9e8d17e706b
Pilou <60597222+PierreLouisF@users.noreply.github.com>
2022-01-11 15:27:29 +0100

    Debut BDD Login

---
+++ MapHighlight.js
@@ -0,0 +1,346 @@
+/*
+	Version : 1
+	Auteur : BRIDE Maxime
+	Date : 12/02/11
+	Contact : macsou@gmail.com
+	
+	Bugs connus :
+	 - Ne g�re pas la propri�t� CSS clip.
+	 - Ne g�re pas la propri�t� CSS position avec la valeur fixed pour l'image.
+	 - Ne g�re pas la propri�t� CSS overflow pour un objet englobant l'image.
+	 
+	A faire :
+	 - R�gler les bugs ci-dessus.
+	 - Afficher r�ellement la bordure des zones area (des cercles ou des polygones au lieu de simples rectangles).
+*/
+
+/*
+	Cette classe permet de mettre en �vidence des zones area d'une map HTML.
+	Param�tres :
+	 - _img : Objet Image repr�sentant l'image dont on veut mettre en �vidence les zones r�actives.
+	 - _persistent : Bool�en permettant de dire si les rectangles de mise en �vidence doivent dispara�tre quand le pointeur n'est plus sur eux.
+	 - _direct : Bool�en permettant de dire si les rectangles de mise en �vidence doivent appara�tre directement ou bien lors du passage de la souris sur la zone.
+	 - _resize : Bool�en permettant de dire si les rectangles de mise en �vidence doivent s'adapter � un �ventuellement changement de taille de l'image.
+*/
+function MapHighlight(_img, _persistent, _direct, _resize) {
+	var img = _img;
+	var persistent = _persistent;
+	var direct = _direct;
+	var resize = _resize;
+	var obj = this;
+	var timer = 0;
+	var rectsContainer = document.createElement('div');
+	rectsContainer.style.position = 'absolute';
+	rectsContainer.style.left = rectsContainer.style.top = '0px';
+	document.body.appendChild(rectsContainer);
+	
+	/*
+	========================================================================
+	=========================== M�thodes priv�es ===========================
+	========================================================================
+	*/
+	
+	/*
+		Permet de trouver la map associ�e � une image.
+		Param�tres :
+		 - img : L'image dont on veut conna�tre la map associ�e.
+	*/
+	var findMap = function(img) {
+		var maps = document.getElementsByTagName('map');
+		for(var i = 0; i < maps.length; i++) {
+			if(maps[i].getAttribute('name') == img.getAttribute('usemap').replace('#', '')) {
+				return maps[i];
+			}
+		}
+		return 0;
+	}
+	
+	/*
+		Permet de supprimer tous les rectangles de mise en �vidence et l'interaction de toutes les zones area d'une map.
+		Param�tres :
+		 - map : La map que l'on veut nettoyer.
+	*/
+	var clearMap = function(map) {
+		var as = rectsContainer.getElementsByTagName('a');
+		var l = as.length;
+		for(var i = 0; i < l; i++) {
+			rectsContainer.removeChild(as[0]);
+		}
+		var areas = map.getElementsByTagName('area');
+		for(var i = 0; i < areas.length; i++) {
+			areas[i].onmouseover = function() {};
+		}
+	}
+			
+	/*
+		Renvoie la position absolue d'un objet.
+		Param�tres :
+		 - obj : Objet HTML dont on veut conna�tre la position absolue.
+	*/
+	var findPos = function(obj) {
+		var l = t = 0;
+		if(obj.offsetParent) {
+			do {
+				l += obj.offsetLeft;
+				t += obj.offsetTop;
+			} while(obj = obj.offsetParent);
+		}
+		return [l, t];
+	}
+	
+	/*
+		Cr�e un rectangle qui dispara�t quand on d�place la souris hors de lui.
+		Param�tres :
+		 - area : Objet HTML repr�sentant la zone area associ�e au rectangle � cr�er.
+	*/
+	var createRect = function(area) {
+		var dim = coordsToDim(area.coords, area.shape);
+		for(var i = 0; i < dim.length; i++) {
+			if(dim[i] == 'Infinity' || isNaN(dim[i])) {
+				return;
+			}
+		}
+		var rect = document.createElement('a');
+		for(var i = 0; i < area.attributes.length; i++) {
+			if(area.attributes[i].name != 'coords' && area.attributes[i].name != 'shape') {
+				rect.setAttribute(area.attributes[i].name, area.getAttribute(area.attributes[i].name));
+			}
+		}
+		rect.style.cssText += area.style.cssText;
+		rect.style.position = 'absolute';
+		rect.style.left = dim[0] + 'px';
+		rect.style.top = dim[1] + 'px';
+		rect.style.width = dim[2] + 'px';
+		rect.style.height = dim[3] + 'px';
+		rect.style.backgroundColor = 'red'; //Important pour IE.
+		rectsContainer.appendChild(rect);
+		area.style.visibility = 'hidden'; //Important pour IE.
+		rect.style.visibility = 'visible'; //Important pour IE.
+		if(!persistent) {
+			rect.onmouseout = function() {
+				rectsContainer.removeChild(this);
+			}
+		}
+	}
+	
+	/*
+		Renvoie les dimensions d'un polygne (position coin haut gauche et position coin bas droite).
+		Param�tres :
+		 - coords : Tableau d'entier repr�sentant les coordonn�es du polygone.
+	*/
+	var dimPoly = function(coords) {
+		var minT, minL, maxT, maxL;
+		minL = maxL = coords[0];
+		minT = maxT = coords[1];		
+		for(var i = 0; i < coords.length; i++) {
+			if(i % 2 == 0) {
+				if(coords[i] < minL) {
+					minL = coords[i];
+				}
+				if(coords[i] > maxL) {
+					maxL = coords[i];
+				}
+			} else {
+				if(coords[i] < minT) {
+					minT = coords[i];
+				}
+				if(coords[i] > maxT) {
+					maxT = coords[i];
+				}
+			}
+		}
+		return [minL, minT, maxL, maxT];
+	}
+	
+	/*
+		Renvoie l'�paisseur de la bordure d'une image.
+		Param�tres :
+		 - img : L'image dont on veut conna�tre l'�paisseur de la bordure.
+	*/
+	var getBorderWidth = function(img) {
+		var width = img.offsetWidth;
+		var bwTmp = img.style.borderWidth;
+		img.style.borderWidth = '0px';
+		var nWidth = img.offsetWidth;
+		var bw = width - nWidth;
+		img.style.borderWidth = bwTmp;
+		return bw / 2;
+	}
+	
+	/*
+		Renvoie la hauteur naturelle (v�ritable) de l'image img.
+		Param�tres :
+		 - img : L'image dont on veut conna�tre la hauteur naturelle.
+	*/
+	var getNaturalHeight = function(img){
+		if(typeof img.naturalHeight == "undefined") {
+			var tmp = new Image();
+			tmp.src = img.src;
+			return tmp.height;
+		} else {
+			return img.naturalHeight;
+		}
+	}
+	
+	/*
+		Renvoie la largeur naturelle (v�ritable) de l'image img.
+		Param�tres :
+		 - img : L'image dont on veut conna�tre la largeur naturelle.
+	*/
+	var getNaturalWidth = function(img){
+		if(typeof img.naturalWidth == "undefined") {
+			var tmp = new Image();
+			tmp.src = img.src;
+			return tmp.width;
+		} else {
+			return img.naturalWidth;
+		}
+	}
+
+	
+	/*
+		Renvoie les dimensions (top, left, width, height) d'une zon area en fonction de son type et de ses coordonn�es.
+		Param�tres :
+		 - coordsStr : Cha�ne de caract�res repr�sentant les coordonn�es de la zone area.
+		 - shape : Cha�ne de caract�res repr�sentant le type de la zone area.
+	*/
+	var coordsToDim = function(coordsStr, shape) {
+		var mapPos = findPos(img);
+		var coords = coordsStr.replace(/ /g, '').split(',');
+		for(var j = 0; j < coords.length; j++) {
+			coords[j] = parseInt(coords[j], 10);
+		}
+		var t, l, w, h;
+		if(shape == 'rect') {
+			l = coords[0] + mapPos[0];
+			t = coords[1] + mapPos[1];
+			w = coords[2] - coords[0];
+			h = coords[3] - coords[1];
+		} else if(shape == 'circle') {
+			l = coords[0] - coords[2] + mapPos[0];
+			t = coords[1] - coords[2] + mapPos[1];
+			w = 2 * coords[2];
+			h = 2 * coords[2];
+		} else if(shape == 'poly') {
+			var dim = dimPoly(coords);
+			l = dim[0] + mapPos[0];
+			t = dim[1] + mapPos[1];
+			w = dim[2] - dim[0];
+			h = dim[3] - dim[1];
+		} else {
+			t = l = w = h = 0;
+		}
+		var bw = getBorderWidth(img);
+		if(resize) {
+			//Calculs dans le cas o� l'image est redimensionn�e.
+			var ratioW = (getNaturalWidth(img)) / (img.offsetWidth - 2 * bw);
+			var ratioH = (getNaturalHeight(img)) / (img.offsetHeight - 2 * bw);
+			var diffL = l - mapPos[0] + bw * ratioW;
+			var diffT = t - mapPos[1] + bw * ratioH;
+			l = Math.round(diffL / ratioW + mapPos[0]);
+			t = Math.round(diffT / ratioH + mapPos[1]);
+			w = Math.round(w / ratioW);
+			h = Math.round(h / ratioH);
+		} else {
+			l += bw;
+			t += bw;
+		}
+		return [l, t, w, h];
+	}
+
+	/*
+		Permet de mettre � jour les rectangles de mise en �vidence � chaque d�placement de l'image associ�e.
+	*/
+	var initInterval = function() {
+		var pos = findPos(img);
+		timer = setInterval(function() {
+			var pos2 = findPos(img);
+			if(pos[0] != pos2[0] || pos[1] != pos2[1]) {
+				obj.update();
+				pos = pos2;
+			}
+		}, 100);
+	}
+	
+	/*
+	========================================================================
+	========================== M�thodes publiques ==========================
+	========================================================================
+	*/
+	
+	/*
+		Permet de mettre en place la mise en �vidence de toutes les zones area de l'image concern�e.
+	*/
+	this.highlight = function() {
+		var map = findMap(img);
+		if(map == 0) return;
+		if(timer == 0) {
+			initInterval();
+		}
+		map.style.display = 'none' //Important pour IE.
+		var areas = map.getElementsByTagName('area');
+		for(var i = 0; i < areas.length; i++) {
+			if(direct) {
+				createRect(areas[i]);
+			} else {
+				areas[i].onmouseover = function() {
+					createRect(this);
+				}
+			}
+		}
+	}
+	
+	/*
+		Permet de renseigner une nouvelle valeur pour la variable img.
+		Param�tres :
+		 - _img : Objet Image repr�sentant la nouvelle valeur de la variable img.
+	*/
+	this.setImg = function(_img) {
+		clearMap(findMap(img));
+		img = _img;
+	}
+
+	/*
+		Permet de renseigner une nouvelle valeur pour la variable persistent.
+		Param�tres :
+		 - _persistent : Bool�en repr�sentant la nouvelle valeur de la variable persistent.
+	*/
+	this.setPersistent = function(_persistent) {
+		persistent = _persistent;
+	}
+
+	/*
+		Permet de renseigner une nouvelle valeur pour la variable direct.
+		Param�tres :
+		 - _direct : Bool�en repr�sentant la nouvelle valeur de la variable direct.
+	*/
+	this.setDirect = function(_direct) {
+		direct = _direct;
+	}
+
+	/*
+		Permet de renseigner une nouvelle valeur pour la variable resize.
+		Param�tres :
+		 - _resize : Bool�en repr�sentant la nouvelle valeur de la variable resize.
+	*/
+	this.setResize = function(_resize) {
+		resize = _resize;
+	}
+	
+	/*
+		Permet de mettre � jour les rectangles de mise en �vidence.
+	*/
+	this.update = function() {
+		clearMap(findMap(img));
+		this.highlight();
+	}
+	
+	/*
+		Permet de supprimer la mise en �vidence sur l'image courante.
+	*/
+	this.destroy = function() {
+		clearMap(findMap(img));
+		clearInterval(timer);
+		timer = 0;
+	}
+}
--- busysen/index.js
+++ busysen/index.js
@@ -1,3 +1,5 @@
+const BSON = require('bson');
+
 /**** Import npm libs ****/
 
 const express = require('express');
@@ -17,6 +19,13 @@ const session = require("express-session")({
 const sharedsession = require("express-socket.io-session");
 const bodyParser = require('body-parser');
 const { body, validationResult } = require('express-validator');
+
+const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+
+const uri = "mongodb+srv://admin:12345@cluster0.r7qlx.mongodb.net/test?retryWrites=true&w=majority"
+const client = new MongoClient(uri);
+main().catch(console.error)
 
 /**** Import project libs ****/
 
@@ -54,7 +63,7 @@ app.get('/', (req, res) => {
   states.printServerStatus();
   states.printProfStatus();
   let test = new Theoden();
-  
+
   // Si l'utilisateur n'est pas connecté
   if (!sessionData.username) {
     res.sendFile(__dirname + '/front/html/login.html');
@@ -102,7 +111,7 @@ app.get('/profil', body('register').isLength({ min: 3 }).trim().escape(), (req, 
 });
 
 
-app.post('/login', body('login').isLength({ min: 3 }).trim().escape(), (req, res) => {
+app.post('/login', body('login').isLength({ min: 3 }).trim().escape(), async (req, res) => {
   const login = req.body.login
   const passorwd = req.body.passorwd
   // Error management
@@ -112,27 +121,16 @@ app.post('/login', body('login').isLength({ min: 3 }).trim().escape(), (req, res
     //return res.status(400).json({ errors: errors.array() });
   } else {
     // Store login
-    console.log("Connexion : ", login," ", passorwd)
-    req.session.username = login;
-    req.session.save()
-    res.redirect('/');
-  }
-});
-
-app.post('/register', body('login').isLength({ min: 3 }).trim().escape(), (req, res) => {
-  const login = req.body.login
-  const passorwd = req.body.passorwd
-  // Error management
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    console.log(errors);
-    //return res.status(400).json({ errors: errors.array() });
-  } else {
-    // Store login
-    console.log("Inscription : ", login," ", passorwd)
-    req.session.username = login;
-    req.session.save()
-    res.redirect('/');
+    console.log(client)
+    if(await signIn(client, {email: login, password: passorwd})) {
+      console.log("Mauvais mdp");
+    }else {
+      console.log("Connexion : ", login, " ", passorwd)
+      req.session.username = login;
+      req.session.save()
+      res.redirect('/');
+    }
+    
   }
 });
 
@@ -146,7 +144,7 @@ app.post('/register', body('register').isLength({ min: 3 }).trim().escape(), (re
     //return res.status(400).json({ errors: errors.array() });
   } else {
     // Store login
-    console.log("Connexion : ", login," ", passorwd)
+    console.log("Inscription : ", login, " ", passorwd)
     req.session.username = login;
     req.session.save()
     res.redirect('/');
@@ -159,7 +157,7 @@ io.on('connection', (socket) => {
     console.log("Etudiant connecté")
   });
 
-  socket.on("Redirection",(data) => {
+  socket.on("Redirection", (data) => {
     socket.emit("Redirection2", data);
   });
 
@@ -168,4 +166,107 @@ io.on('connection', (socket) => {
 http.listen(4201, () => {
   console.log('Serveur lancé sur le port 4201');
 });
+
+async function main() {
+  // we'll add code here soon
+  
+  try {
+      await client.connect();
+      //console.log(client)
+      await listDatabases(client);
+      await newReservation(client,{
+          name : "C401",
+          reservations : {
+              start: 12345,
+              end : 54321,
+              idClient: "fnjdjnfsd",
+              _id: new BSON.ObjectId()
+      }})
+      //await signUp(client, {email: "testazjaz@afbazf", password: "zjehfzek"});
+      //console.log(await getLevelData(client,"1"))
+      //console.log(await getDataUser(client,"fnjdjnfsd"))
+
+  } catch (e) {
+      console.error(e);
+  }
+
+}
+
+async function listDatabases(client){
+  databasesList = await client.db().admin().listDatabases();
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+async function getLevelData(client, level){
+  const date = Date.now()
+  //console.log(date)
+  const room = [];
+  const cursor =  await client.db("Projet-Info").collection("Rooms").find({level:level});
+  const result  = await cursor.toArray();
+
+  for(let i of result){
+      let occupied = false;
+      for(let j of i.reservations){
+          console.log("test")
+          console.log(i)
+          if(j.start <= date && j.end >= date) {
+              occupied = true;
+              break;
+          }
+      }
+      if(!occupied) room.push(i.name)
+  }
+
+  return room;
+}
+
+async function getDataUser(client,idClient){
+  const cursor = await client.db("Projet-Info").collection("Rooms").find({'reservations.idClient': idClient});
+  return await cursor.toArray();
+}
+
+async function getDataRoom(client, roomNumber){
+  return await client.db("Projet-Info").collection("Rooms").findOne({ number:roomNumber});
+}
+
+async function newReservation(client, data){
+  /*
+  data : {
+  number : "C956",
+  reservation : {
+      start: 12345,
+      end : 54321,
+      id: ObjectId("61dd4152058f9c92376dea7b")         // id utilisateur
+  }
+  */
+  const roomData = await client.db("Projet-Info").collection("Rooms").findOne({ name: data.name});
+  //console.log(roomData)
+  roomData.reservations.push(data.reservations);
+  //console.log(roomData)
+  if(!roomData) return -1;
+  const result  = await client.db("Projet-Info").collection("Rooms").updateOne({number:data.number},{$set:roomData})
+  return 0;
+}
+
+async function removeReservation(client, idRoom,idReservations){
+  //const result = await client.db("Projet-Info").collection("Rooms").update({ _id : idRoom },{$pull : {reservations: {_id:idReservations}})
+}
+
+async function signIn(client,data){
+  const user = await client.db("Projet-Info").collection("Users").findOne({ email : data.email});
+  const match = await bcrypt.compare(data.password, user.password);
+  if(!match) return -1;
+  //se loger
+  return 0;
+}
+
+async function signUp(client,data){
+  data.password = await bcrypt.hash(data.password,10);
+  const result = await client.db("Projet-Info").collection("Users").findOne({ email : data.email});
+  if(result) return -1;
+  await client.db("Projet-Info").collection("Users").insertOne(data);
+  //se loger
+  return 0;
+}
 
